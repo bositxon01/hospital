522eb40ccdacf2a1f82b15e9472901330615af2a
user <nazirqulovbarkamol5@gmail.com>
2025-02-06 19:46:09 +0500

    Email verification and Employee Controller are created

---
+++ src/main/java/hospital/hospital_system/payload/EmployeeDTO.java
@@ -0,0 +1,35 @@
+package hospital.hospital_system.payload;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmployeeDTO {
+    private Integer id;
+
+    @NotBlank(message = "Firstname is required")
+    private String firstName;
+
+    @NotBlank(message = "Lastname is required")
+    private String lastName;
+
+    @Past(message = "Birthdate should be past date")
+    private LocalDate birthDate;
+
+    @NotBlank(message = "Specialization is required")
+    private String specialization;
+
+    public EmployeeDTO(String firstName, String lastName, LocalDate birthDate, String specialization) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.specialization = specialization;
+    }
+}
