213bd272a277f4bb24aedbe9e47f77a41c906b62
Abdujalil <abdujalilturgunov1@gmail.com>
2025-02-08 19:53:14 +0500

    some config added for Front

--- src/main/java/hospital/hospital_system/config/WebConfig.java
+++ src/main/java/hospital/hospital_system/config/WebConfig.java
@@ -1,8 +1,10 @@
 package hospital.hospital_system.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
